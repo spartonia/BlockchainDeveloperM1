0e9486a4a972c52a712118e58cbcfc8594196ff6
Saeed Partonia <sd_prtn@yahoo.com>
2018-08-19 15:38:53 +0200

    Rename "data" -> "body"

--- server.js
+++ server.js
@@ -13,32 +13,32 @@ let blockchain = new Blockchain();
 
 
 app.get('/height', async (req, res) => {
-	blockchain
-		.getBlockHeight()
-		.then(h=>res.send('h:' + h))
-		.catch((err) => res.sendStatus(404));
+  blockchain
+    .getBlockHeight()
+    .then(h=>res.send('h:' + h))
+    .catch((err) => res.sendStatus(404));
 });
 
 app.get('/block/:id', async (req, res) => {
-	const { id } = req.params;
-	blockchain
-		.getBlock(id)
-		.then(block => res.send(block))
-		.catch((err) => res.status(404).send(`Block #${id} not found.`));
+  const { id } = req.params;
+  blockchain
+    .getBlock(id)
+    .then(block => res.send(block))
+    .catch((err) => res.status(404).send(`Block #${id} not found.`));
 });
 
 app.post('/block', async (req, res) => {
-	const { data } = req.body
-	if (!data) return res.status(400).send('"data" is required.')
+  const { body } = req.body
+  if (!body) return res.status(400).send('"body" is required.')
 
-	blockchain
-		.addBlock(new Block(data))
-		.then(key => {
-			blockchain.getBlock(key)
-				.then(block => res.send(block))
-				.catch((err) => res.status(404).send(err))
-	})
-	.catch((err) => res.status(500).send(e));
+  blockchain
+    .addBlock(new Block(body))
+    .then(key => {
+      blockchain.getBlock(key)
+        .then(block => res.send(block))
+        .catch((err) => res.status(404).send(err))
+  })
+  .catch((err) => res.status(500).send(e));
 });
 
 const PORT = 8000;
